0627b032734b15b75275cd0615d53b6102367a97
THG-Bfreitas <87115315+THG-Bfreitas@users.noreply.github.com>
2022-03-10 20:45:46 -0800

    Update Rename-pc2.ps1

--- Rename-pc2.ps1
+++ Rename-pc2.ps1
@@ -1,7 +1,10 @@
 $originalname = [System.Net.Dns]::GetHostName()
+$username = “rampart\thgadmin”
+$password = “n@kaJl3O0q&v"
+$Credentials = New-Object System.Management.Automation.PSCredentical $Username,$Password
 $hilbpcname = get-content C:\hilb\Hilb-pcname.txt
 write-host $originalname is the original name
 write-host Renaming pc to $hilbpcname
-$username = $env:UserName
-write-host $username
-Rename-Computer -NewName $hilbpcname
+
+Rename-Computer -NewName "newname" -DomainCredential $Credentials -Force -restart
+
