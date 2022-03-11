0bce8ee5a0e366da9a9e72a25015be2151e88bff
THG-Bfreitas <87115315+THG-Bfreitas@users.noreply.github.com>
2022-03-10 21:45:23 -0800

    Update THG-Rename.ps1

--- THG-Rename.ps1
+++ THG-Rename.ps1
@@ -1,6 +1,6 @@
 
 $username = "rampart\thgadmin"
-$password = ConvertTo-SecureString "n@kaJl3O0qv" -AsPlainText -Force
+$password = ConvertTo-SecureString "" -AsPlainText -Force
 $Credentials = new-object System.Management.Automation.PSCredential($username, $password)
 
 Rename-Computer -NewName rbp-l-dlupo -DomainCredential $Credentials -Force
