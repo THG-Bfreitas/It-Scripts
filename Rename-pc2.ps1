00846678c74f67a0e1517192649bc2032614ccc7
THG-Bfreitas <87115315+THG-Bfreitas@users.noreply.github.com>
2022-03-10 20:28:00 -0800

    Update Rename-pc2.ps1

--- Rename-pc2.ps1
+++ Rename-pc2.ps1
@@ -2,4 +2,6 @@ $originalname = [System.Net.Dns]::GetHostName()
 $hilbpcname = get-content C:\hilb\Hilb-pcname.txt
 write-host $originalname is the original name
 write-host Renaming pc to $hilbpcname
+$username = $env:UserName
+write-host $username
 Rename-Computer -NewName $hilbpcname
