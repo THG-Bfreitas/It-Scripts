5a631d02a4e7e13410fcb0e305509fe6f0a496dc
THG-Bfreitas <87115315+THG-Bfreitas@users.noreply.github.com>
2022-03-10 21:42:03 -0800

    Update THG-Rename.ps1

--- THG-Rename.ps1
+++ THG-Rename.ps1
@@ -1,30 +1,6 @@
-#dynamic computer rename
-$THGDomainuser = $(Get-WMIObject -class Win32_ComputerSystem | select username).username | split-path -Leaf
-$THGDomainuser
-$Dontuse =  "system"
-new-item C:\hilb -itemtype directory -ErrorAction SilentlyContinue
-$currentpcname = [System.Net.Dns]::GetHostName()
-#Set the Agency 3 Digit Code
-$THGSHORT =   "rpb"
-cls
-If((gwmi win32_computersystem -ea 0).pcsystemtype -ne 2)
-{Write-Host "Machine is a Desktop" 
-$computertype = "-D-" }
-Else
-{Write-Host "Machine is a Laptop" 
-$computertype = "-L-" }
 
-if(-not($THGDomainuser -eq $Dontuse)){
+$username = "rampart\thgadmin"
+$password = ConvertTo-SecureString "n@kaJl3O0qv" -AsPlainText -Force
+$Credentials = new-object System.Management.Automation.PSCredential($username, $password)
 
- Write-host "$($THGDomainuser) is being used as the username" }
- else
- { Write-host Found Invalid Username $dontuse Exiting script
-EXIT 
-}
-
-$thgnew = $THGSHORT+$computertype+$THGDomainuser
-$thgnew | Out-file C:\hilb\Hilb-pcname.txt
-write-host $currentpcname is the current pc name
-write-host $thgnew is being set as computer name. Comptuer name will change on reboot.
-
-#Rename-Computer -NewName $THGNew
+Rename-Computer -NewName rbp-l-dlupo -DomainCredential $Credentials -Force
