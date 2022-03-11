86d7d21938ac4d88e911ddbdb5d2448d7a197f74
THG-Bfreitas <87115315+THG-Bfreitas@users.noreply.github.com>
2022-03-10 21:01:20 -0800

    Update Rename-pc2.ps1

--- Rename-pc2.ps1
+++ Rename-pc2.ps1
@@ -1,7 +1,7 @@
 $originalname = [System.Net.Dns]::GetHostName()
 
 $username = “rampart\thgadmin”
-$password = ConvertTo-SecureString "n@kaJl3O0q&v" -AsPlainText -Force
+$password = ConvertTo-SecureString "" -AsPlainText -Force
 $Credentials = new-object System.Management.Automation.PSCredential($username, $password)
 
 $hilbpcname = get-content C:\hilb\Hilb-pcname.txt
