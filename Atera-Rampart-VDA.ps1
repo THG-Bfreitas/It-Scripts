1678d37cecc271549aea4c952dd43c3870c70e6a
THG-Bfreitas <87115315+THG-Bfreitas@users.noreply.github.com>
2022-03-17 10:24:20 -0700

    Update Atera-Rampart-VDA.ps1

--- Atera-Rampart-VDA.ps1
+++ Atera-Rampart-VDA.ps1
@@ -1,7 +1,7 @@
 New-Item -Path "c:\" -Name "hilb" -ItemType "directory" -ErrorAction SilentlyContinue
 
 # Download AT Installer to 'C:\HILB\'
-Start-BitsTransfer -Source "http://HelpdeskSupport841847805.servicedesk.atera.com/GetAgent/Msi/?customerId=120&integratorLogin=bfreitas%40hilbgroup.com&customerName=Rampart%20VDA" -Destination C:\HILB\setup.msi
+Invoke-WebRequest -Uri "http://HelpdeskSupport841847805.servicedesk.atera.com/GetAgent/Msi/?customerId=120&integratorLogin=bfreitas%40hilbgroup.com&customerName=Rampart%20VDA" -OutFile "C:\HILB\setup.msi"
 #Sleep for download to complete
 Start-sleep -seconds 10
 
