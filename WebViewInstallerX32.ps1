8359f34ec27be5d49e0a7fd2e5e3a88cf2372fec
THG-Bfreitas <87115315+THG-Bfreitas@users.noreply.github.com>
2022-06-07 08:26:08 -0700

    Update WebViewInstallerX32.ps1

--- WebViewInstallerX32.ps1
+++ WebViewInstallerX32.ps1
@@ -1,5 +1,5 @@
 $DownloadURL = "https://eusthginfrastructure.blob.core.windows.net/thg-remediation-scripts/MicrosoftEdgeWebView2RuntimeInstallerX86.exe"
-Write-host Waiting for download to finish
+Write-host Starting Webview 2 Runtime 32bit installer download.
 Start-BitsTransfer -Source $DownloadURL -Destination "C:\hilb\MicrosoftEdgeWebView2RuntimeInstallerX86.exe" 
 Start-Sleep -Seconds 60
 Start-Process  "C:\HILB\MicrosoftEdgeWebView2RuntimeInstallerX86.exe"  -ArgumentList "/silent /install"
