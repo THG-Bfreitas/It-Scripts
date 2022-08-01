a15dc399c7a42b4ac90f61938d49333ea30cec38
THG-Bfreitas <87115315+THG-Bfreitas@users.noreply.github.com>
2022-08-01 12:15:20 -0700

    Update adobereader.ps1

--- adobereader.ps1
+++ adobereader.ps1
@@ -1,11 +1,10 @@
-#webview webview 2 -32bit (not 64)
 $software =  "Acrobat Reader*"
 $installed = (Get-ItemProperty HKLM:\SOFTWARE\Classes\Installer\Products\* | Where { $_.ProductName -match $software }) -ne $null
 If(-Not $installed) {
 Write-Host "'$software' NOT is installed."
 Write-host "Downloading $($software)"
 $DownloadURL = "https://eusthginfrastructure.blob.core.windows.net/thg-remediation-scripts/AcroRdrDC2200120169_en_US.exe"
-Invoke-RestMethod -Uri $DownloadURL -OutFile "C:\hilb\MicrosoftEdgeWebView2RuntimeInstallerX86.exe" 
+Invoke-RestMethod -Uri $DownloadURL -OutFile "C:\HILB\AcroRdrDC2200120169_en_US.exe" 
 Start-Process  "C:\HILB\AcroRdrDC2200120169_en_US.exe"  -ArgumentList "/sAll /rs /msi EULA_ACCEPT=YES"
 Write-host "MicrosoftEdgeWebView2RuntimeInstallerX86.exe Installation completed"
 } else { Write-host $software already installed }
