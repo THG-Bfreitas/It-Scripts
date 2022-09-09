be0cef091af8592645d8d81f51fed0c99d4e6701
THG-Bfreitas <87115315+THG-Bfreitas@users.noreply.github.com>
2022-09-09 09:51:04 -0700

    Create localadmin.ps1

---
+++ localadmin.ps1
@@ -0,0 +1,16 @@
+# powershell -command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/THG-Bfreitas/It-Scripts/main/localadmin.ps1'))" 
+$OSVersion = (get-itemproperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ProductName).ProductName
+Write-host Checking if computer is a server
+If($OSVersion -ilike "*Server*")
+{
+Write-Host "Stopping install It's Server!"
+exit
+}
+Else
+{
+cls
+Write-Host "Okay, Not A server"
+}
+Write-host "Adding user to Administrators group"
+
+Add-LocalGroupMember -Member "$($env:USERDNSDOMAIN)\$($env:USERNAME)" -Group Administrators 
