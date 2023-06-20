dddf3d6446d0418f2be54d64c04500abebe23ac6
THG-Bfreitas <87115315+THG-Bfreitas@users.noreply.github.com>
2023-06-20 12:24:09 -0700

    Update zoomperuser.ps1

--- zoomperuser.ps1
+++ zoomperuser.ps1
@@ -1,26 +1,9 @@
-
-# powershell -command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/THG-Bfreitas/It-Scripts/main/zoomperuser.ps1'))" 
-# uninstall 
 # Create the folder C:\hilb\exclude if it doesn't exist
 $excludeFolder = "C:\hilb\exclude"
 if (-not (Test-Path -Path $excludeFolder -PathType Container)) {
     New-Item -Path $excludeFolder -ItemType Directory | Out-Null
     Write-Host "Created folder: $excludeFolder"
 }
-
-# Specify the starting directory
-$startingDirectory = "C:\Users\"
-
-# Specify the filename to search for
-$filename = "zoom.exe"
-
-# Specify the log file path
-$logFile = Join-Path -Path $excludeFolder -ChildPath "HilbRemediation.log"
-
-$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-$logMessage = "$timestamp - $filename searhing in directory: $($startingDirectory)"
-Write-Host $logMessage
-Write-Output $logMessage | Out-File -Append -FilePath $logFile
 
 
 # Recursive function to search for the file and move the folder, logging the operations with timestamps
@@ -55,7 +38,66 @@ function SearchAndMoveFile($directory, $filename) {
     foreach ($subdirectory in $subdirectories) {
         SearchAndMoveFile $subdirectory.FullName $filename
     }
+
 }
+
+
+# Specify the starting directory
+$startingDirectory = "C:\Users\"
+
+#Searching for zoom
+$filename = "zoom.exe"
+
+
+# Specify the log file path
+$logFile = Join-Path -Path $excludeFolder -ChildPath "HilbRemediation.log"
+
+$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+$logMessage = "$timestamp - $filename searching in directory: $($startingDirectory)"
+Write-Host $logMessage
+Write-Output $logMessage | Out-File -Append -FilePath $logFile
+
 
 # Start the search from the specified directory
 SearchAndMoveFile $startingDirectory $filename
+
+$logMessage = "$timestamp - $filename search completed"
+Write-Host $logMessage
+Write-Output $logMessage | Out-File -Append -FilePath $logFile
+
+#Searching for Chrome
+$filename = "chrome.exe"
+
+# Specify the log file path
+$logFile = Join-Path -Path $excludeFolder -ChildPath "HilbRemediation.log"
+
+$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+$logMessage = "$timestamp - $filename searching in directory: $($startingDirectory)"
+Write-Host $logMessage
+Write-Output $logMessage | Out-File -Append -FilePath $logFile
+SearchAndMoveFile $startingDirectory $filename
+
+
+$logMessage = "$timestamp - $filename search completed"
+Write-Host $logMessage
+Write-Output $logMessage | Out-File -Append -FilePath $logFile
+
+
+#Searching for firefox
+$filename = "firefox.exe"
+
+
+# Specify the log file path
+$logFile = Join-Path -Path $excludeFolder -ChildPath "HilbRemediation.log"
+
+$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+$logMessage = "$timestamp - $filename searching in directory: $($startingDirectory)"
+Write-Host $logMessage
+Write-Output $logMessage | Out-File -Append -FilePath $logFile
+
+SearchAndMoveFile $startingDirectory $filename
+
+
+$logMessage = "$timestamp - $filename search completed"
+Write-Host $logMessage
+Write-Output $logMessage | Out-File -Append -FilePath $logFile
